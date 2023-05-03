c3d74af2925a845d334a62de02ca3231948953c3
kayvann <kayvan.najafzadeh@gmail.com>
2023-05-03 15:54:47 -0700

    use of fragment

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,19 @@
+// we can wrap with a div, shorthand is with "wrap with abbriviation"
+// use of fragment imported
+// use of empty angle brackets is a shorthand for fragment
+
 function ListGroup() {
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </>
   );
 }
 
