2c2624159c36cf342efc785e10214061fade03ab
kayvann <kayvan.najafzadeh@gmail.com>
2023-05-03 16:52:19 -0700

    creating callback prop

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,15 @@ import ListGroup from "./components/ListGroup";
 function App() {
   var cities = ["San Francisco", "New York", "Los Angles"];
 
+  const handleSelectItem = (item: string) => console.log(item);
+
   return (
     <div>
-      <ListGroup items={cities} heading="Cities" />
+      <ListGroup
+        items={cities}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
