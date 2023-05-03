bbbb2967974459c789ea1e12b8bdf7d988bf9676
kayvann <kayvan.najafzadeh@gmail.com>
2023-05-03 16:41:16 -0700

    useState to make the selections active

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,32 @@
-// Handling Click events
-// make sure to pass the function reference to onClick
+// State
+// each instance of the component has its own state using useState
+// Hook
+// const arr = useState(-1); initialize
+// arr[0]; // variable
+// arr[1]; // updater function
+
+import { useState } from "react";
 
 function ListGroup() {
   var items = ["San Francisco", "New York", "Los Angles"];
 
-  const handleClick = (event: MouseEvent) => console.log(event);
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>Empty Items</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => setSelectedIndex(index)}
+          >
             {item}
           </li>
         ))}
