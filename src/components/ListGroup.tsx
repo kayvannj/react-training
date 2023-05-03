247128c5189a7a7d8be9a509ea0d387174961f0f
kayvann <kayvan.najafzadeh@gmail.com>
2023-05-03 16:03:10 -0700

    create a list and show dynamic data in it

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,18 @@
-// we can wrap with a div, shorthand is with "wrap with abbriviation"
-// use of fragment imported
-// use of empty angle brackets is a shorthand for fragment
+// make sure you have key for each item in the list
+// there are no for loops in jsx so we need to use map instead
 
 function ListGroup() {
+  const items = ["San Francisco", "New York", "Los Angles"];
+
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
