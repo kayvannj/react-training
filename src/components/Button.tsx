412ddc06a513cac112913cd73c2c3092720ac48b
kayvann <kayvan.najafzadeh@gmail.com>
2023-05-03 17:20:59 -0700

    adding a button and making it dynamic using limited colors

---
+++ src/components/Button.tsx
@@ -0,0 +1,27 @@
+// use ? to make a prop optional
+// use the default value in the prop
+// use the | to make provide specific options
+
+interface Props {
+  children: string;
+  onClick: () => void;
+  color?:
+    | "primary"
+    | "secondary"
+    | "success"
+    | "danger"
+    | "warning"
+    | "info"
+    | "light"
+    | "dark"
+    | "link";
+}
+const Button = ({ children, onClick, color = "primary" }: Props) => {
+  return (
+    <button className={"btn btn-" + color} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
