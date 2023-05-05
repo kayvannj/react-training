c0f4ae53cb646496690d73ac9b4969f32c56845a
kayvann <kayvan.najafzadeh@gmail.com>
2023-05-05 10:37:17 -0700

    modifying state object exersise 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
-import Like from "./components/Like/Like";
+import Like from "./components/Like";
 
 function App() {
-  const [showAlert, setShowAlert] = useState(false);
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: "John",
+    },
+  });
 
-  let alert = (
-    <Alert onDismiss={() => setShowAlert(false)}>
-      Hello <span>World</span>
-    </Alert>
-  );
+  const handleClick = () => {
+    setGame({ ...game, player: { ...game.player, name: "Kayvan" } });
+  };
 
   return (
     <div>
-      {showAlert && alert}
-      <Button onClick={() => setShowAlert(true)} color="primary">
-        Submit
+      <p>{game.player.name}</p>
+      <Button onClick={handleClick} color="primary">
+        change name
       </Button>
-      <Like onClick={() => console.log("clicked")}></Like>
     </div>
   );
 }
