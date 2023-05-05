d14f74a8656dfd02643987e8b83428b6dcd87b1c
kayvann <kayvan.najafzadeh@gmail.com>
2023-05-05 09:42:09 -0700

    using style module

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,7 @@
 // use ? to make a prop optional
 // use the default value in the prop
 // use the | to make provide specific options
+import styles from "./Button.module.css";
 
 interface Props {
   children: string;
@@ -18,7 +19,10 @@ interface Props {
 }
 const Button = ({ children, onClick, color = "primary" }: Props) => {
   return (
-    <button className={"btn btn-" + color} onClick={onClick}>
+    <button
+      className={[styles.btn, styles["btn-" + color]].join(" ")}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
