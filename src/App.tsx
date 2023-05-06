81416664072aed1fdbbb8dde0e69377666e70d94
kayvann <kayvan.najafzadeh@gmail.com>
2023-05-05 17:13:06 -0700

    create a form and submit it

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
-import Like from "./components/Like";
-import ListGroup from "./components/ListGroup/ListGroup";
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <>
-      <ExpandableText maxChar={100}>Lorem ipsum dolor</ExpandableText>
+      <Form></Form>
     </>
   );
 }
